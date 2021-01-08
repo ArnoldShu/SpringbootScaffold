13ca45cc1f634caa3691968a4952cea72796e89c
arnold <409196007@qq.com>
2021-01-08 15:31:52 +0800

    springboot 自定义starter集成

---
+++ 11_Integration-Starter/src/main/java/com/arnold/www/Application.java
@@ -0,0 +1,20 @@
+package com.arnold.www;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @ClassName com.arnold.www.Application
+ * @Description: 启动器
+ * @Author Arnold
+ * @Date 2020/4/15
+ * @Version V2.0
+ **/
+@SpringBootApplication
+public class Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Application.class, args);
+    }
+
+}
